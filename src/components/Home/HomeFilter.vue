255124842226ce774b8e9b43861e12e50a22db5a
dahyeon405 <dahyeon405@naver.com>
2023-03-15 17:25:38 +0900

    fix: 기존 필터링 상태 반영되도록 수정

--- src/components/Home/HomeFilter.vue
+++ src/components/Home/HomeFilter.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CATEGORY from '@/consts/CATEGORY'
+import { filterStore } from '@/store/filterStore'
 
 const emit = defineEmits(['categoryClicked'])
 
@@ -9,20 +10,18 @@ const handleCategoryCick = (event: MouseEvent) => {
 
   const selected = target.id
   emit('categoryClicked', selected)
-
-  target.classList.toggle('bg-gray-600')
-  target.classList.toggle('bg-[#204FF5]')
 }
 </script>
 
 <template>
   <div class="flex justify-center my-10">
-    <div class="flex flex-wrap gap-x-3 w-5/6 gap-y-3 justify-center">
+    <div class="flex flex-wrap gap-x-3 w-5/6 gap-y-3 justify-center" ref="">
       <div
         v-for="(category, index) in CATEGORY"
         :key="index"
         :id="category"
-        class="flex justify-center items-center w-24 h-24 bg-[#204FF5] rounded-xl text-lg font-bold text-white cursor-pointer"
+        class="flex justify-center items-center w-24 h-24 rounded-xl text-lg font-bold text-white cursor-pointer"
+        :class="filterStore.data.has(category) ? 'bg-[#204FF5]' : 'bg-gray-600'"
         @click="handleCategoryCick"
       >
         {{ category }}
